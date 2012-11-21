7e2d39131d36a5da9468d0ff01cbcf058e460171
Tomas Gregor <tomas.gregor@me.com>
2012-11-21 17:21:18 -0600

    TG: removed index.html from public

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harpuun::Application.config.secret_token = 'f76b81ca9e90ae493782969c0dfb3ea5e37429cfa991bb9ce6f2946d2ddb0ce463cc9ac6aab85f9e11cbe2745e43f2829deeb6df53046554be6ab7eeb301ee1b'
