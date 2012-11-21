6f183f7ee6d7eb89c73b2b3b6b31fb078fc56b9c
Tomas Gregor <tomas.gregor@me.com>
2012-11-21 17:50:57 -0600

    TG: updated contractor model with has_secure password

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,3 +1,6 @@
 class Contractor < ActiveRecord::Base
-  attr_accessible :avatar_url, :company, :email, :first_name, :last_name, :password_digest
+  attr_accessible :avatar_url, :company, :email, :first_name, :last_name, :password, :password_confirmation
+  
+  has_secure_password
+  
 end
