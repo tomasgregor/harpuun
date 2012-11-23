1aa0d4dd2ae56e191834649afb8c091ca4e63e4e
Tomas Gregor <tomas.gregor@me.com>
2012-11-23 14:47:20 -0600

    TG: added complete Carrierwave and Rmagick functionality to Contractor model

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,5 +1,5 @@
 class Contractor < ActiveRecord::Base
-  attr_accessible :avatar_url, :company, :email, :first_name, :last_name, :password, :password_confirmation
+  attr_accessible :avatar_url, :remote_avatar_url_url, :company, :email, :first_name, :last_name, :password, :password_confirmation
   
   has_secure_password
   
