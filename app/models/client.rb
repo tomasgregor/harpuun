54506645a7bb0020da750d6e790a33365c30de0c
Tomas Gregor <tomas.gregor@me.com>
2012-11-28 15:55:05 -0600

    TG: added SignUp validations to Starter and Client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,12 +1,17 @@
 class Client < ActiveRecord::Base
   attr_accessible :avatar, :remote_avatar_url, :company, :email, :first_name, :last_name, :password, :password_confirmation
   
-  has_many :projects
+  validates :first_name, :presence => true, :length => { :minimum => 2 }
+  validates :last_name, :presence => true, :length => { :minimum => 2 }
+
+  validates :email, 
+            :presence => true, 
+            :uniqueness => true,
+            :email_format => true
+  
+  validates :password, :length => { :in => 6..20 }
   
   has_secure_password
-  
-  validates_uniqueness_of :email
-  
   mount_uploader :avatar, ImageUploader
   
 end
--- app/models/starter.rb
+++ app/models/starter.rb
@@ -1,10 +1,18 @@
 class Starter < ActiveRecord::Base
   attr_accessible :avatar, :remote_avatar_url, :class_id1, :class_id2, :class_id3, :email, :first_name, :last_name, :password, :password_confirmation, :quarter, :skill_id1, :skill_id2, :skill_id3, :year
-    
+  
+  
+  validates :first_name, :presence => true, :length => { :minimum => 2 }
+  validates :last_name, :presence => true, :length => { :minimum => 2 }
+
+  validates :email, 
+            :presence => true, 
+            :uniqueness => true,
+            :email_format => true
+  
+  validates :password, :length => { :in => 6..20 }
+
   has_secure_password
-  
-  validates_uniqueness_of :email
-  
   mount_uploader :avatar, ImageUploader
   
 end
