4ea6f6325497ec87fc372a1ecef9525650a89a0b
Tomas Gregor <tomas.gregor@me.com>
2012-12-01 16:28:55 -0600

    TG: defined association between Project and Offer

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :client_id, :deadline, :description, :name, :reward, :skill_id1, :skill_id2, :skill_id3, :url
   
   belongs_to :client
+  has_many :offers
   
   before_save :offer_project_to_starter
   
