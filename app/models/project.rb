594c7c0a72e36f4b2e0b654d2c6432877b686a52
Tomas Gregor <tomas.gregor@me.com>
2012-11-30 20:15:37 -0600

    TG: added a callback method which picks eligible Starter who will be offer to accept newly created Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,14 @@ class Project < ActiveRecord::Base
   
   belongs_to :client
   
+  before_save :offer_project_to_starter
+  
+  def offer_project_to_starter
+    not_eligible_starters_ids = Project.select(:offered_to).map(&:offered_to)
+    all_starters_ids = Starter.select(:id).map(&:id)
+    eligible_starters_ids = all_starters_ids - not_eligible_starters_ids
+    offered_starter_id = eligible_starters_ids.shuffle[0]
+    self.offered_to = offered_starter_id
+  end
+  
 end
