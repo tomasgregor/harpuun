a2d6415135f8c17bd3113056ac61188c5538f380
Tomas Gregor <tomas.gregor@me.com>
2012-12-05 16:53:06 -0600

    TG: optimized Client and Starter SignUp validations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require_tree .
+
+
+$('.dropdown-toggle').dropdown()
--- app/models/starter.rb
+++ app/models/starter.rb
@@ -2,11 +2,10 @@ class Starter < ActiveRecord::Base
   attr_accessible :avatar, :remote_avatar_url, :class_id1, :class_id2, :class_id3, :email, :first_name, :last_name, :password, :password_confirmation, :quarter, :skill_id1, :skill_id2, :skill_id3, :year
   
   
-  validates :first_name, :presence => true, :length => { :minimum => 2 }
-  validates :last_name, :presence => true, :length => { :minimum => 2 }
+  validates :first_name, :length => { :minimum => 2 }
+  validates :last_name, :length => { :minimum => 2 }
 
-  validates :email, 
-            :presence => true, 
+  validates :email,
             :uniqueness => true,
             :email_format => true
   
