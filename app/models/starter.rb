dd3dc46d364619c63129ad1da1ca82e5d7f99169
Tomas Gregor <tomas.gregor@me.com>
2012-12-06 11:33:23 -0600

    TG: changes to be merged

--- app/models/starter.rb
+++ app/models/starter.rb
@@ -1,5 +1,5 @@
 class Starter < ActiveRecord::Base
-  attr_accessible :avatar, :remote_avatar_url, :class_id1, :class_id2, :class_id3, :email, :first_name, :last_name, :password, :password_confirmation, :quarter, :skill_id1, :skill_id2, :skill_id3, :year
+  attr_accessible :avatar, :remote_avatar_url, :class_id1, :class_id2, :class_id3, :email, :first_name, :last_name, :password, :password_confirmation, :quarter, :skill_id1, :skill_id2, :skill_id3, :year, :twitter
   
   
   validates :first_name, :length => { :minimum => 2 }
