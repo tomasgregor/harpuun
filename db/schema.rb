b422a2feb21abdcd56c911830fb5825e21f6a951
Tomas Gregor <tomas.gregor@me.com>
2012-12-05 18:29:36 -0600

    TG: Starter SignUp design enhancement - includes changes in CSS files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201222637) do
+ActiveRecord::Schema.define(:version => 20121205230312) do
 
   create_table "clients", :force => true do |t|
     t.string   "first_name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(:version => 20121201222637) do
     t.integer  "class_id3"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "twitter"
   end
 
 end
