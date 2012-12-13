d7eb01b7f9f2710245e129f0fb16c4ebeef56776
Tomas Gregor <tomas.gregor@me.com>
2012-12-12 21:05:26 -0600

    TG: enhanced Project Index and Show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
     session[:starter_id].present?
   end
   
+  def skill_name(skill_id)
+    name = Skill.find_by_id(skill_id).name
+  end
+  
+  def self.shorten(length)
+    truncate(self, :length => length)
+  end
+  
 end
